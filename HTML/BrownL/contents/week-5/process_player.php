c4c90fe937bd42dd4f9f7af10c9b2321f7311881
Lance <lance.brown42@gmail.com>
2020-09-28 10:02:37 -0400

    assignment 8 works

--- HTML/BrownL/contents/week-5/process_player.php
+++ HTML/BrownL/contents/week-5/process_player.php
@@ -10,22 +10,23 @@
 		$txtZip = $_POST["txtZip"];
 		$decAge = $_POST["txtAge"];
 		$txtGender = $_POST["txtGender"];
-		$txtAgeRange = NULL;
+		$txtAgeRange = "";
 
 		if ($decAge < 18) {
-			$txtAgeRange = "Junior"
+			$txtAgeRange = "Junior";
 		} elseif($decAge < 55){
-			$txtAgeRange = "Professional"
-		} elseif($decAge >= 55){
-			$txtAgeRange = "Senior"
+			$txtAgeRange = "Professional";
+		} else{
+			$txtAgeRange = "Senior";
 		}
 		
 		?>
 		Name: <?php echo $txtName;?><br>
 		Address: <?php echo $txtAddress;?><br>
-		City:<?php echo $txtCity;?><br>
-		Zip:<?php echo $txtZip;?><br>
-		Player Type:<?php echo $txtGender + " " + $txtAgeRange;?><br>
+		City: <?php echo $txtCity;?><br>
+		Zip: <?php echo $txtZip;?><br>
+		Player Type: <?php echo $txtGender . " " . $txtAgeRange;?><br>
+		
 		
 	</body>
 </html>
